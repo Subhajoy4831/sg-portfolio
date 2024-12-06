5e77cb0310cdd1146b29853d23efd56d2787b1f6
Subhajoy Ghosh <ghoshsubhajoy02@gmail.com>
2024-12-07 01:10:23 +0530

    resume fix

--- app/components/Header.js
+++ app/components/Header.js
@@ -43,7 +43,7 @@ const Header = () => {
             <li><NavLink href="#contact">Contact</NavLink></li>
             <li>
               <motion.a
-                href="/resume.pdf"
+                href="https://drive.google.com/file/d/1X5B0BGWzVm316irD_A5SvYS5wbPdPXC1/view?usp=drive_link"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="bg-blue-500 text-white px-4 py-2 rounded-full font-semibold text-sm hover:bg-blue-600 transition duration-300"
